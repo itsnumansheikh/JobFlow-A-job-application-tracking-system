64f8b2d1217ce5733a04bf974b4cc263c49ad2bb
Numan Sheikh <nomansheikh2664@gmailcom>
2026-09-24 00:45:28 +0600

    dev auth-service

--- auth-service/app.js
+++ auth-service/app.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const authRoutes = require('./routes/auth');
+const { requireAuth } = require('./middleware/auth');
 
 const app = express();
 app.use(express.json());
 app.use('/auth', authRoutes);
+
+app.get('/auth/me', requireAuth, (req, res) => {
+  res.json({ message: 'You are authenticated', user: req.user });
+});
 
 module.exports = app;
